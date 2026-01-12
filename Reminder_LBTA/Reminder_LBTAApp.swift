04ee2e19007b46aa4733b2eb61b2228408838767
iMaxiOS <ukr.maksim1@gmail.com>
2026-01-12 13:21:08 +0200

    added project

---
+++ Reminder_LBTA/Reminder_LBTAApp.swift
@@ -0,0 +1,17 @@
+//
+//  Reminder_LBTAApp.swift
+//  Reminder_LBTA
+//
+//  Created by Maxim Hranchenko on 12.01.2026.
+//
+
+import SwiftUI
+
+@main
+struct Reminder_LBTAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
